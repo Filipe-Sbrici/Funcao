d768cf7f7b7f7a6bff9e846a57fb5af9d7919310
Caua-Alborghette <cauaalborghette09@gmail.com>
2025-04-29 11:38:34 -0300

    C 0.2

--- 1/calcular.php
+++ 1/calcular.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 1</title>
+    <title>Exercício 4</title>
     <link href="styles.css" rel="stylesheet" >
 </head>
 <body>
 <div class = "chickenjockey">
-<h1>CALCULO DE MULTIPLICAÇÃO</h1>
+<h1>ÍMPAR OU PAR</h1>
 <form method="post">
     <button type="submit" name="sortear">Girar</button>
 </form>
