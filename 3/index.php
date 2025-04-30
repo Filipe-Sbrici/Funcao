7999c807600e078e9eef0a4b1080bf8d5e721fba
Caua-Alborghette <cauaalborghette09@gmail.com>
2025-04-29 21:07:41 -0300

    c 0.7

--- 3/index.php
+++ 3/index.php
@@ -3,29 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 2</title>
+    <title>Exercício 3</title>
     <link href="styles.css" rel="stylesheet" >
 </head>
 <body>
 <div class = "chickenjockey">
-<h1>íMPAR OU PAR</h1>
+<h1>MAIOR NÚMERO</h1>
 
 <form method="post">
     Valor 1: <input type="number" name="num1" required><br>
+    Valor 2: <input type="number" name="num2" required><br>
+    Valor 3: <input type="number" name="num3" required><br>
     <button type="submit">Multiplicar</button>
 </form>
 <?php
  
- function parOuImpar($num1) {
-    if ($num1 % 2 == 0) 
-        {return "par";} 
-    else {return "ímpar";}
+ function maiorNumero($nuns) {
+    sort ($nuns);
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $num1 = $_POST["num1"];
-    $res = parOuImpar($num1);
-    echo "<p>O número $num1 é $res.</p>";
+    $nuns = [$_POST["num1"], $_POST["num2"], $_POST["num3"]];
+    echo "<p>O maior número é $nuns[0].</p>";
 }
 ?>
 
